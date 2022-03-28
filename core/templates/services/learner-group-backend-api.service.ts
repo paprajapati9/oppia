f028febc20d765b7dd74272879b20b3cef07b8c2
paprajapati9 <paprajapati9@gmail.com>
2022-03-28 18:07:57 +0530

    fronent changes

---
+++ core/templates/services/learner-group-backend-api.service.ts
@@ -0,0 +1,24 @@
+
+// interface LearnerGroupInfoBackendDict {
+//     learnerGroupSummaries: LearnerGroupSummaryBackendDict[];
+// }
+
+// export class LearnerGroupService {
+  
+//     /**
+//      * Returns a promise that resolves to a list of learner group summaries.
+//      */
+//     async getLearnerGroupInfoAsync(){
+        
+//     }
+//   }
+
+//   /**
+//    * It will have function getUsersAllLearnerGroupsAsync() that makes a GET request at /userlearnergroupshandler/ endpoint and returns a dictionary of learnerGroupSummaries and learnerGroupInvitationSummaries. The function will not take any arguments.
+//     It has a function getLearnerGroupInfoAsync() that makes a GET request at /learnergrouphandler/<groupId> endpoint and returns a dictionary of learnerGroupSummaries and learnerGroupInvitationSummaries. The function will not take any arguments.
+// It will have a function createNewLearnerGroupAsync() which creates a new learner group, it takes in groupTitle, groupDescription, invitations, syllabus as arguments to create the learner group and returns the groupId of the learner group created.
+// It will have a function updateLearnerGroupAsync() which updates the learner group’s info, it takes groupId, groupTitle, groupDescription, invitations, syllabus as arguments and returns success or failure message.
+// It will have a function deleteLearnerGroupAsync() which takes groupId as an argument and deletes that learner group.
+// It will have function 
+//    * 
+//    */
